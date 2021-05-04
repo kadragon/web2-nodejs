df13ec8c34a1136072d261ac4d1938b5e197d091
kangdonguk <kangdongouk@gmail.com>
2021-05-04 23:51:45 +0900

    8

---
+++ syntax/Template.js
@@ -0,0 +1,5 @@
+const name = "egoing";
+console.log(
+  `${name} 
+  Qui amet voluptate officia magna dolor ipsum do. Laboris nostrud irure in amet excepteur enim culpa. Quis eiusmod elit non incididunt dolore. Nulla anim voluptate tempor voluptate occaecat irure. Dolor ad ad cupidatat commodo ipsum adipisicing magna eu in ullamco.`,
+);
--- syntax/Variable.js
+++ syntax/Variable.js
@@ -4,5 +4,8 @@ a = 2;
 console.log(a);
 // 1 = 2; comment
 const name = "egoing";
-const letter = `Nulla ${name} laborum et deserunt velit et nulla enim deserunt. Duis voluptate aliquip anim esse nulla laborum esse qui. Pariatur anim deserunt Lorem magna est ad commodo nisi excepteur deserunt in velit. Reprehenderit dolore cupidatat consectetur tempor laboris incididunt nisi magna deserunt sint consectetur deserunt pariatur fugiat. Quis dolore ipsum ea veniam officia do in officia consequat cillum do culpa irure et. Consequat enim irure et consectetur ea nostrud proident nostrud irure ipsum voluptate.`;
+const letter =
+  `Nulla ` +
+  name +
+  ` laborum et deserunt velit et nulla enim deserunt. Duis voluptate aliquip anim esse nulla laborum esse qui. Pariatur anim deserunt Lorem magna est ad commodo nisi excepteur deserunt in velit. Reprehenderit dolore cupidatat consectetur tempor laboris incididunt nisi magna deserunt sint consectetur deserunt pariatur fugiat. Quis dolore ipsum ea veniam officia do in officia consequat cillum do culpa irure et. Consequat enim irure et consectetur ea nostrud proident nostrud irure ipsum voluptate.`;
 console.log(letter);
