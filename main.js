8639f1131d07164b9bd31bd68289ff5a8d579e48
kangdonguk <kangdongouk@gmail.com>
2021-05-04 23:32:28 +0900

    5 - Node.js로 웹서버 만들기

---
+++ main.js
@@ -0,0 +1,22 @@
+import http from "http";
+import fs from "fs";
+import path from "path";
+
+const __dirname = path.resolve();
+
+const app = http.createServer(function (request, response) {
+  let url = request.url;
+  if (request.url === "/") {
+    url = "/index.html";
+  }
+  if (request.url === "/favicon.ico") {
+    return response.writeHead(404);
+  }
+
+  response.writeHead(200);
+
+  console.log(__dirname + url);
+  response.end(fs.readFileSync(__dirname + url));
+});
+
+app.listen(3000);
