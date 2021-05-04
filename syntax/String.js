d5fbf39b7c7fb9be5fdd0557ad745a0742a02474
kangdonguk <kangdongouk@gmail.com>
2021-05-04 23:42:32 +0900

    6.2

---
+++ syntax/String.js
@@ -0,0 +1,5 @@
+console.log("1" + `1`);
+console.log(
+  `Do cillum nulla cillum in ex officia sit. Elit laborum exercitation commodo ea occaecat duis consequat adipisicing pariatur mollit. Pariatur qui labore reprehenderit ea adipisicing sunt ipsum sint reprehenderit adipisicing sit anim. Non consectetur eu proident laboris duis.`
+    .length,
+);
