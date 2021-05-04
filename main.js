f4660fe732b5fdeaed5bbc68233fcd8a1d15b5f8
kangdonguk <kangdongouk@gmail.com>
2021-05-05 00:13:44 +0900

    11

--- main.js
+++ main.js
@@ -8,16 +8,32 @@ const __dirname = path.resolve();
 const app = http.createServer(function (request, response) {
   let _url = request.url;
   const queryData = url.parse(_url, true).query;
-  console.log(queryData);
-  if (_url === "/") {
-    _url = "/index.html";
-  } else if (_url === "/favicon.ico") {
+  const title = queryData.id !== undefined ? queryData.id : "Welcome";
+  if (_url === "/favicon.ico") {
     return response.writeHead(404);
   }
 
   response.writeHead(200);
-  // response.end(fs.readFileSync(__dirname + _url));
-  response.end(queryData.query);
+
+  let template = `<!doctype html>
+<html>
+<head>
+  <title>WEB2 - ${title}</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1><a href="/">WEB</a></h1>
+  <ol>
+    <li><a href="/?id=HTML">HTML</a></li>
+    <li><a href="/?id=CSS">CSS</a></li>
+    <li><a href="/?id=JavsScript">JavaScript</a></li>
+  </ol>
+  <h2>${title}</h2>
+</body>
+</html>
+`;
+
+  response.end(template);
 });
 
 app.listen(3000);
