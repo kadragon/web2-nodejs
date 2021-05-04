490e2e36e702e9f3d5d767c3bd458aa0c6742a17
kangdonguk <kangdongouk@gmail.com>
2021-05-05 00:02:12 +0900

    10

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
 import http from "http";
 import fs from "fs";
+import url from "url";
 
 import path from "path";
 const __dirname = path.resolve();
 
 const app = http.createServer(function (request, response) {
-  let url = request.url;
-  if (request.url === "/") {
-    url = "/index.html";
-  }
-  if (request.url === "/favicon.ico") {
+  let _url = request.url;
+  const queryData = url.parse(_url, true).query;
+  console.log(queryData);
+  if (_url === "/") {
+    _url = "/index.html";
+  } else if (_url === "/favicon.ico") {
     return response.writeHead(404);
   }
 
   response.writeHead(200);
-
-  console.log(__dirname + url);
-  response.end(fs.readFileSync(__dirname + url));
+  // response.end(fs.readFileSync(__dirname + _url));
+  response.end(queryData.query);
 });
 
 app.listen(3000);
