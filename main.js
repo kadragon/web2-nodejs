dc61ad046066a733835818d9a9bf033d550af5be
kangdonguk <kangdongouk@gmail.com>
2021-05-05 10:43:50 +0900

    19

--- main.js
+++ main.js
@@ -2,19 +2,8 @@ import http from "http";
 import url from "url";
 import { readFile } from "node:fs";
 
-const app = http.createServer(function (request, response) {
-  const _url = request.url;
-  const queryData = url.parse(_url, true).query;
-  const pathName = url.parse(_url, true).pathname;
-  const title = queryData.id;
-
-  if (_url === "/favicon.ico") {
-    return response.writeHead(404);
-  }
-
-  if (pathName === "/") {
-    readFile(`data/${title}`, "utf-8", (err, data) => {
-      let template = `<!doctype html>
+function makeContents(title, description) {
+  const template = `<!doctype html>
 <html>
 <head>
   <title>WEB2 - ${title}</title>
@@ -28,14 +17,38 @@ const app = http.createServer(function (request, response) {
     <li><a href="/?id=JavaScript">JavaScript</a></li>
   </ol>
   <h2>${title}</h2>
-  ${data}
+  ${description}
 </body>
 </html>
 `;
+  return template;
+}
+
+const app = http.createServer(function (request, response) {
+  const _url = request.url;
+  const queryData = url.parse(_url, true).query;
+  const pathName = url.parse(_url, true).pathname;
+
+  if (_url === "/favicon.ico") {
+    return response.writeHead(404);
+  }
+
+  if (pathName === "/") {
+    if (queryData.id === undefined) {
+      const title = "Welcome";
+      const description = "Hello! Node.js";
 
       response.writeHead(200);
-      response.end(template);
-    });
+      response.end(makeContents(title, description));
+    } else {
+      const title = queryData.id;
+      readFile(`data/${title}`, "utf-8", (err, description) => {
+        if (err) throw err;
+
+        response.writeHead(200);
+        response.end(makeContents(title, description));
+      });
+    }
   } else {
     response.writeHead(404);
     response.end("Not found");
